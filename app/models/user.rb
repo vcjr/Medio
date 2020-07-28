3976c9887d47cbd6d89ed2de081e3a695c9decae
Victor Crispin <victorrcrispinjr@gmail.com>
2020-07-28 09:19:57 -0400

    Created backend user authentication setup methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,12 +16,40 @@ class User < ApplicationRecord
   validates :email, :session_token, uniqueness: true
   validates :password, length: { minimum: 8 }, allow_nil: true
 
-  def self.find_by_credentials 
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil unless user && user.is_password?(password)
+    user
+  end
+
+  def is_password?(password)
+    bcrypt_password = BCrypt::Password.new(self.password_digest)
+    bcrypt_password.is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
   end
 
   def self.generate_session_token
+    SecureRandom:urlsafe_base64
   end
-
-
 
 end
