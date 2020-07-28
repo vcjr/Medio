06a56aed93fbfad93ff14f447c30d728a2581c15
Victor Crispin <victorrcrispinjr@gmail.com>
2020-07-28 08:59:56 -0400

    Added user controller, api::users routes and validation for backend user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  name            :string           not null
+#  short_bio       :string
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+  validates :email, :name, :password_digest, :session_token, presence: true
+  validates :email, :session_token, uniqueness: true
+  validates :password, length: { minimum: 8 }, allow_nil: true
+
+  def self.find_by_credentials 
+  end
+
+  def self.generate_session_token
+  end
+
+
+
 end
