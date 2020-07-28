cb5d1f14e781edf1423096e41f02e9e10f7257ba
Victor Crispin <victorrcrispinjr@gmail.com>
2020-07-28 11:24:38 -0400

    Finished testing and creating session api utility functions

--- frontend/medio.jsx
+++ frontend/medio.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+//! TESTING ONLY ~ REMOVE AFTER DONE !!!!!!!
+import { signup, login, logout} from './util/session_api_util'
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   
+  //! TESTING ONLY ~ REMOVE AFTER DONE !!!!!!!
+  window.signup = signup;
+  window.login = login;
+  window.logout = logout;
+  //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
   ReactDom.render(<h1>Medio Is Up</h1>, root)
 });
