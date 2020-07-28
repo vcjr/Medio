29e9a402f4a4008121f9c544ca682c8742e6a926
Victor Crispin <victorrcrispinjr@gmail.com>
2020-07-28 13:17:12 -0400

    Finished the session errors reducer

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,2 +1,16 @@
 import { RECEIVE_SESSION_ERRORS, RECEIVE_CURRENT_USER } from '../actions/session_actions';
 
+const sessionErrorsReducer = (currentState = [], action) => {
+  Object.freeze(currentState);
+
+  switch (action.type) {
+    case RECEIVE_SESSION_ERRORS:
+      return action.errors;
+    case RECEIVE_CURRENT_USER:
+      return [];
+    default:
+      return currentState;
+  }
+};
+
+export default sessionErrorsReducer;
