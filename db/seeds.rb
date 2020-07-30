5cd4627cfee467cee5352d9ec82bc5e2ff15ecff
Victor Crispin <victorrcrispinjr@gmail.com>
2020-07-30 16:23:26 -0400

    Created Demouser in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.delete_all
 
-User.create(email: "demouser@mail.com", )
+demoUser = User.create!(
+  email: 'demouser@email.com',
+  password: "hunter1234",
+  name: 'demouser'
+)
