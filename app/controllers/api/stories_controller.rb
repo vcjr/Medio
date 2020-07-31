55b66a0491e1561351b48c928a891a9832e7a244
Victor Crispin <victorrcrispinjr@gmail.com>
2020-07-31 16:40:31 -0400

    Setting up route and controller

--- app/controllers/api/stories_controller.rb
+++ app/controllers/api/stories_controller.rb
@@ -1,2 +1,17 @@
 class Api::StoriesController < ApplicationController
+    def show
+      @story = Story.new(story_params)
+    end
+
+    def create
+    end
+
+    
+
+    private
+
+    def story_params
+      
+    end
+
 end
---
+++ app/views/api/stories/_story.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! story, :id, :author_id, :title, :subtitle, :body, :title_draft, :body_draft, :published, :published_date
+
