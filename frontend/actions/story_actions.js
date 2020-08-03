f3539a53119aefbf6275538405d70e83a777c76f
Victor Crispin <victorrcrispinjr@gmail.com>
2020-08-03 10:31:04 -0400

    Finished creating story actions and thunk actions.

--- frontend/actions/story_actions.js
+++ frontend/actions/story_actions.js
@@ -3,7 +3,59 @@ import * as StoryAPIUtil from '../util/stories_api_util';
 // NOTE: Constants
 export const RECEIVE_STORY = "RECEIVE_STORY";
 export const RECEIVE_STORIES = "RECEIVE_STORIES";
+export const RECEIVE_STORY_ERRORS = 'RECEIVE_STORY_ERRORS';
 
 // NOTE: Regular Action Creators
 
+const receiveStory = story => ({
+  type: RECEIVE_STORY,
+  story
+});
+
+const receiveStories = stories => ({
+  type: RECEIVE_STORIES,
+  stories
+});
+
+const receiveErrors = errors => ({
+  type: RECEIVE_STORY_ERRORS,
+  errors
+});
+
 // NOTE: Thunk Action Creators
+
+export const createStory = story => dispatch => {
+  return StoryAPIUtil.createStory(story)
+    .then(story => dispatch(receiveStory(story)),
+      errors => (dispatch(receiveErrors(errors.responseJSON)))
+    );
+};
+
+export const updateStory = story => dispatch => {
+  return StoryAPIUtil.updateStory(story)
+    .then(story => dispatch(receiveStory(story)),
+      errors => (dispatch(receiveErrors(errors.responseJSON)))
+    );
+};
+
+export const deleteStory = storyId => dispatch => {
+  return StoryAPIUtil.deleteStory(storyId)
+    .then(() => dispatch(receiveStory(story)),
+      errors => (dispatch(receiveErrors(errors.responseJSON)))
+    );
+};
+
+export const fetchStory = storyId => dispatch => {
+  return StoryAPIUtil.fetchStory(storyId)
+    .then(story => dispatch(receiveStory(story)),
+      errors => (dispatch(receiveErrors(errors.responseJSON)))
+    );
+};
+
+//NOTE: I have not set any filters yet so this will return all of the stories inside of the database
+export const fetchStories = () => dispatch => {
+  return StoryAPIUtil.fetchStories()
+    .then(stories => dispatch(receiveStories(stories)),
+      errors => (dispatch(receiveErrors(errors.responseJSON)))
+    );
+};
